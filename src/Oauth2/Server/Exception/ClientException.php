9eb1896d5426ec93276f10b4b72be6cc3d86dc59
lvqiang <lvqiang@yongche.com>
2013-12-10 16:22:33 +0800

    add client Exception and oauth2 Exception

---
+++ src/Oauth2/Server/Exception/ClientException.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+* @file ClientException.php
+* @Function Client Exception 
+* @author lvqiang, lvqiang@yongche.com
+* @version 0.1
+* @date 2013-12-10
+ */
+
+class ClientException extends Exception {
+}
---
+++ src/Oauth2/Server/Exception/Oauth2Exception.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+* @file Oauth2Exception.php
+* @Function Oauth2 Exception 
+* @author lvqiang, lvqiang@yongche.com
+* @version 0.1
+* @date 2013-12-10
+ */
+
+class Oauth2Exception extends Exception {
+}
